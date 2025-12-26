68b9cc8b703cc547fd6fb9f231caa64e57cd7d14
nouh <nouh.nowile@gmail.com>
2025-12-26 22:34:38 +0100

    modify the interface: add more methods to use inside menu class

--- Emesapp/Menus/Interface.php
+++ Emesapp/Menus/Interface.php
@@ -6,4 +6,7 @@ interface MenuMainFunctions
 {
     static  function clearScreen();
     static function printHeader(string $title);
+    static  function confirmAction(string $message): bool;
+    static  function input(string $label, bool $required = true): string;
+     static function printMenu(array $options, string $title = "MENU");
 }
